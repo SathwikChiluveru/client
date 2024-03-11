c1d6b1d805fa2e7641fa9b344b0abc09d109f58b
Sathwik Chiluveru <51926084+SathwikChiluveru@users.noreply.github.com>
2024-03-11 17:26:31 +0800

    sidebar + navbar changes #1

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,5 +1,5 @@
-'use client'
-
+'use client';
+import React, { useState } from 'react';
 import {
   IconButton,
   Avatar,
@@ -19,41 +19,48 @@ import {
   MenuDivider,
   MenuItem,
   MenuList,
-} from '@chakra-ui/react'
+} from '@chakra-ui/react';
 import {
   FiHome,
   FiStar,
   FiSettings,
   FiMenu,
-  FiBell,
-  FiChevronDown,
-  FiPieChart,
   FiUsers,
   FiLogOut,
-  FiFileText
-} from 'react-icons/fi'
+  FiFileText,
+  FiPieChart,
+  FiChevronDown
+} from 'react-icons/fi';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+} from '@chakra-ui/react';
+import "@fontsource/lexend-deca"; 
+import { color } from 'framer-motion';
 
 const LinkItems = [
-    { name: 'Home', icon: FiHome },
-    { name: 'Settings', icon: FiPieChart },
-    {
-      name: 'Users',
-      icon: FiUsers,
-      subItems: [
-        { name: 'View All', icon: FiStar },
-        { name: 'Admins', icon: FiSettings },
-      ],
-    },
-    {
-      name: 'Logs',
-      icon: FiFileText,
-      subItems: [
-        { name: 'View All', icon: FiStar },
-        { name: 'My Activity', icon: FiSettings },
-      ],
-    },
-    { name: 'Sign Out', icon: FiLogOut },
-  ];
+  { name: 'Home', icon: FiHome },
+  { name: 'Settings', icon: FiPieChart },
+  {
+    name: 'Users',
+    icon: FiUsers,
+    subItems: [
+      { name: 'View All', icon: FiStar },
+      { name: 'Admins', icon: FiSettings },
+    ],
+  },
+  {
+    name: 'Logs',
+    icon: FiFileText,
+    subItems: [
+      { name: 'View All', icon: FiStar },
+      { name: 'My Activity', icon: FiSettings },
+    ],
+  },
+  { name: 'Sign Out', icon: FiLogOut },
+];
 
 const SidebarContent = ({ onClose, ...rest }) => {
   return (
@@ -67,54 +74,63 @@ const SidebarContent = ({ onClose, ...rest }) => {
       h="full"
       {...rest}>
       <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
-        <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
+        <Text fontSize="2xl" fontFamily="Lexend Deca" fontWeight="bold">
           Ascenda
         </Text>
         <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
       </Flex>
-      {LinkItems.map((link) => (
-        <NavItem key={link.name} icon={link.icon}>
-          {link.name}
-        </NavItem>
+      {LinkItems.map((link, index) => (
+        <React.Fragment key={link.name}>
+            {index === LinkItems.length - 1 && <Box h="40" />}
+            <NavItem icon={link.icon} subItems={link.subItems}>
+                {link.name}
+            </NavItem>
+      </React.Fragment>
       ))}
     </Box>
-  )
-}
+  );
+};
 
 const NavItem = ({ icon, children, subItems, ...rest }) => {
-    return (
-      <Box
-        as="span"
-        style={{ textDecoration: 'none' }}
-        _focus={{ boxShadow: 'none' }}>
-        {subItems ? (
-          <Menu>
-            <MenuButton
-              as={Flex}
-              align="center"
-              p="4"
-              mx="4"
-              borderRadius="lg"
-              role="group"
-              cursor="pointer"
-              _hover={{
-                bg: '#1A1E43',
-                color: 'white',
-              }}
-              {...rest}>
-              {icon && (
-                <Icon
-                  mr="4"
-                  fontSize="16"
-                  _groupHover={{
-                    color: 'white',
-                  }}
-                  as={icon}
-                />
-              )}
-              {children}
-              <FiChevronDown />
-            </MenuButton>
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <Box as="span" style={{ textDecoration: 'none' }} _focus={{ boxShadow: 'none' }}>
+      {subItems ? (
+        <Menu>
+          <MenuButton
+            as={Flex}
+            align="center"
+            p="4"
+            mx="4"
+            borderRadius="lg"
+            role="group"
+            cursor="pointer"
+            _hover={{
+              bg: '#1A1E43',
+              color: 'white',
+            }}
+            onClick={toggleDropdown}
+            {...rest}
+          >
+            {icon && (
+              <Icon
+                mr="4"
+                fontSize="16"
+                _groupHover={{
+                  color: 'white',
+                }}
+                as={icon}
+              />
+            )}
+            {children}
+            <FiChevronDown />
+          </MenuButton>
+          {isOpen && (
             <MenuList>
               {subItems.map((subItem) => (
                 <MenuItem key={subItem.name} as="a" href="#">
@@ -122,118 +138,128 @@ const NavItem = ({ icon, children, subItems, ...rest }) => {
                 </MenuItem>
               ))}
             </MenuList>
-          </Menu>
-        ) : (
-          <Box
-            as="a"
-            href="#"
-            _focus={{ boxShadow: 'none' }}>
-            <Flex
-              align="center"
-              p="4"
-              mx="4"
-              borderRadius="lg"
-              role="group"
-              cursor="pointer"
-              _hover={{
-                bg: '#1A1E43',
-                color: 'white',
-              }}
-              {...rest}>
-              {icon && (
-                <Icon
-                  mr="4"
-                  fontSize="16"
-                  _groupHover={{
-                    color: 'white',
-                  }}
-                  as={icon}
-                />
-              )}
-              {children}
-            </Flex>
-          </Box>
-        )}
-      </Box>
-    );
+          )}
+        </Menu>
+      ) : (
+        <Box
+          as="a"
+          href="#"
+          _focus={{ boxShadow: 'none' }}
+        >
+          <Flex
+            align="center"
+            p="4"
+            mx="4"
+            borderRadius="lg"
+            role="group"
+            cursor="pointer"
+            _hover={{
+              bg: '#1A1E43',
+              color: 'white',
+            }}
+            {...rest}
+          >
+            {icon && (
+              <Icon
+                mr="4"
+                fontSize="16"
+                _groupHover={{
+                  color: 'white',
+                }}
+                as={icon}
+              />
+            )}
+            {children}
+          </Flex>
+        </Box>
+      )}
+    </Box>
+  );
 };
 
 const MobileNav = ({ onOpen, ...rest }) => {
-  return (
-    <Flex
-      ml={{ base: 0, md: 60 }}
-      px={{ base: 4, md: 4 }}
-      height="20"
-      alignItems="center"
-      bg={useColorModeValue('white', 'gray.900')}
-      borderBottomWidth="1px"
-      borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
-      justifyContent={{ base: 'space-between', md: 'flex-end' }}
-      {...rest}>
-      <IconButton
-        display={{ base: 'flex', md: 'none' }}
-        onClick={onOpen}
-        variant="outline"
-        aria-label="open menu"
-        icon={<FiMenu />}
-      />
+    return (
+      <Flex
+        ml={{ base: 0, md: 60 }}
+        px={{ base: 4, md: 4 }}
+        height="20"
+        alignItems="center"
+        bg={useColorModeValue('white', 'gray.900')}
+        borderBottomWidth="1px"
+        borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
+        justifyContent={{ base: 'space-between', md: 'flex-end' }}
+        {...rest}
 
-      <Text
-        display={{ base: 'flex', md: 'none' }}
-        fontSize="2xl"
-        fontFamily="monospace"
-        fontWeight="bold">
-        Logo
-      </Text>
+      >
+        <HStack spacing={{ base: '0', md: '6' }}>
+          <Flex alignItems={'center'}>
+            {/* Breadcrumb in MobileNav */}
+            <Box mr="630">
+                <Breadcrumb fontWeight="medium" fontSize="sm">
+                <BreadcrumbItem>
+                    <BreadcrumbLink href="#">HOME</BreadcrumbLink>
+                </BreadcrumbItem>
 
-      <HStack spacing={{ base: '0', md: '6' }}>
-        <IconButton size="lg" variant="ghost" aria-label="open menu" icon={<FiBell />} />
-        <Flex alignItems={'center'}>
-          <Menu>
-            <MenuButton py={2} transition="all 0.3s" _focus={{ boxShadow: 'none' }}>
-              <HStack>
-                <Avatar
-                  size={'sm'}
-                  src={
-                    'https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                  }
-                />
-                <VStack
-                  display={{ base: 'none', md: 'flex' }}
-                  alignItems="flex-start"
-                  spacing="1px"
-                  ml="2">
-                  <Text fontSize="sm">Justina Clark</Text>
-                  <Text fontSize="xs" color="gray.600">
-                    Admin
-                  </Text>
-                </VStack>
-                <Box display={{ base: 'none', md: 'flex' }}>
-                  <FiChevronDown />
-                </Box>
-              </HStack>
-            </MenuButton>
-            <MenuList
-              bg={useColorModeValue('white', 'gray.900')}
-              borderColor={useColorModeValue('gray.200', 'gray.700')}>
-              <MenuItem>Profile</MenuItem>
-              <MenuItem>Settings</MenuItem>
-              <MenuItem>Billing</MenuItem>
-              <MenuDivider />
-              <MenuItem>Sign out</MenuItem>
-            </MenuList>
-          </Menu>
-        </Flex>
-      </HStack>
-    </Flex>
-  )
-}
+                <BreadcrumbItem>
+                    <BreadcrumbLink href="#">About</BreadcrumbLink>
+                </BreadcrumbItem>
+
+                <BreadcrumbItem isCurrentPage>
+                    <BreadcrumbLink href="#">Current</BreadcrumbLink>
+                </BreadcrumbItem>
+                </Breadcrumb>
+            </Box>
+            
+            <Menu>
+              <MenuButton py={2} transition="all 0.3s" _focus={{ boxShadow: 'none' }}>
+                <HStack>
+                  <VStack
+                    display={{ base: 'none', md: 'flex' }}
+                    alignItems="flex-start"
+                    spacing="0px"
+                    ml="10" mr="10"
+                  >
+                    <Text fontSize={18} fontFamily="Lexend Deca">Neil Sharma</Text>
+                    <Text fontSize="xs" color="gray.600" fontFamily="Lexend Deca">
+                      Owner
+                    </Text>
+                  </VStack>
+                  <Box display={{ base: 'none', md: 'flex' }}>
+                  </Box>
+                </HStack>
+              </MenuButton>
+              <MenuList
+                bg={useColorModeValue('white', 'gray.900')}
+                borderColor={useColorModeValue('gray.200', 'gray.700')}
+              >
+                <MenuItem>Profile</MenuItem>
+                <MenuItem>Settings</MenuItem>
+                <MenuItem>Billing</MenuItem>
+                <MenuDivider />
+                <MenuItem>Sign out</MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
+        </HStack>
+  
+        <IconButton
+          display={{ base: 'flex', md: 'none' }}
+          onClick={onOpen}
+          variant="outline"
+          aria-label="open menu"
+          icon={<FiMenu />}
+        />
+      </Flex>
+    );
+};
+  
 
 const Landing = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
+      {/* <Navbar /> */}
       <SidebarContent onClose={() => onClose} display={{ base: 'none', md: 'block' }} />
       <Drawer
         isOpen={isOpen}
@@ -252,7 +278,7 @@ const Landing = () => {
         {/* Content */}
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
